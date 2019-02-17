8eac7282b4650ee81de3724cb096f9a09e425da0
CherishMoore <cherishmoore@byui.edu>
2019-02-16 19:04:45 -0700

    Lesson 6

---
+++ assignments/lesson6/script/Hamburger.js
@@ -0,0 +1,7 @@
+/*function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+]*/
+
+function toggleMenu() {
+	document.getElementById("primaryNav").classList.toggle("hide");
+}
