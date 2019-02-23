5c419caea9b5e1b69c4b6edb69e6aa02aaec8d4d
CherishMoore <cherishmoore@byui.edu>
2019-02-23 12:45:02 -0700

    complete7

--- assignments/lesson7/script/windchill.js
+++ assignments/lesson7/script/windchill.js
@@ -1,11 +1,9 @@
-function doInputOutput() {
-    var tempF = 14;
-    var speed = 7;
-    var result = windChill(speed, tempF);
-    document.getElementById('chill').innerHTML = result;
-    }
-function windChill(speed,tempF) {
-    var speed = Math.pow(speed, .16);
-    result = 35.74 + 0.6215 * tempF - 35.75 * speed + 0.4275 * tempF * speed + " Fahrenheit";
-    return result;
-    }
+function windchill() {
+    var tempF = (66 + 14) / 2;
+    var speed = Math.pow(7, 0.16);
+    var chill = 35.74 + (0.6215 * tempF) - (35.75 * speed) + (0.4275 * tempF * speed);
+    chill = Math.round(chill * 10) / 10 + ' &deg F'
+    return chill;
+}
+
+document.getElementById("windChill").innerHTML = windchill();
