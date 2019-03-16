25d557947687d7c789152868327255adc03045ad
CherishMoore <cherishmoore@byui.edu>
2019-03-15 23:16:29 -0600

    windchill

--- assignments/lesson10/script/curr-weather.js
+++ assignments/lesson10/script/curr-weather.js
@@ -14,16 +14,14 @@ weatherObj.onload =  function () {
     
 
 }
-var weatherChill = new XMLHttpRequest();
-weatherChill.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=ad29c6aa79f2ea1e6701e2107e4be543',true); 
-weatherChill.send();
 
-weatherChill.onload = function windchill() {
-    var weatherData = JSON.parse(weatherChill.responseText);
 
-    var maxTemp = document.getElementById('max-city').innerHTML = weatherData.main.temp_max;
-    var minTemp = document.getElementById('min-city').innerHTML = weatherData.main.temp_min;
-    var wind = document.getElementById('wind-city').innerHTML = weatherData.wind.speed;
+function windchill() {
+    
+
+    var maxTemp = document.getElementById('max-city').innerHTML;
+    var minTemp = document.getElementById('min-city').innerHTML;
+    var wind = document.getElementById('wind-city').innerHTML;
     var tempF = (maxTemp + minTemp) / 2;
     var speed = Math.pow(wind, 0.16);
     var chill = 35.74 + (0.6215 * tempF) - (35.75 * speed) + (0.4275 * tempF * speed);
